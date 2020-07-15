053bc1015cd6cda3c6a1771a16313a0cb583e103
rabah babaci <4rabah@gmail.com>
2020-07-15 09:29:34 -0700

    create the NavBar component

--- app/src/components/NavBar/index.js
+++ app/src/components/NavBar/index.js
@@ -1,5 +1,24 @@
 import React from "react";
+import { Link } from "react-scroll";
 
 export default function NavBar() {
-  return <div></div>;
+  return (
+    <div className="navBar">
+      <Link to="home" smooth={true} duration={400}>
+        HOME
+      </Link>
+      <Link to="about" smooth={true} duration={600}>
+        ABOUT ME
+      </Link>
+      <Link to="help" smooth={true} duration={600}>
+        HOW I HELP
+      </Link>
+      <Link to="resources" smooth={true} duration={600}>
+        RESOURCES
+      </Link>
+      <Link to="contact" smooth={true} duration={600}>
+        CONTACT
+      </Link>
+    </div>
+  );
 }
