1c3d05360768a68588551d4a6fcf4fb8b8650fdf
rabah babaci <4rabah@gmail.com>
2020-07-15 10:11:11 -0700

    adjust the navBar when scrolling

--- app/src/App.js
+++ app/src/App.js
@@ -3,7 +3,7 @@ import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App home">
+    <div className="App">
       <NavBar />
 
       <div className="about">
--- app/src/components/NavBar/index.js
+++ app/src/components/NavBar/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-scroll";
+import { Link, animateScroll as scroll } from "react-scroll";
 
 import "./navBar.scss";
 
@@ -7,19 +7,29 @@ export default function NavBar() {
   return (
     <div className="navBar">
       <div className="desktop">
-        <Link to="home" smooth={true} duration={400}>
+        <button
+          onClick={() =>
+            scroll.scrollToTop({
+              duration: 400,
+            })
+          }
+        >
           HOME
-        </Link>
-        <Link to="about" smooth={true} duration={600}>
+        </button>
+
+        <Link to="about" spy={true} smooth={true} duration={600}>
           ABOUT ME
         </Link>
-        <Link to="help" smooth={true} duration={600}>
+
+        <Link to="help" spy={true} smooth={true} duration={600}>
           HOW I HELP
         </Link>
-        <Link to="resources" smooth={true} duration={600}>
+
+        <Link to="resources" spy={true} smooth={true} duration={600}>
           RESOURCES
         </Link>
-        <Link to="contact" smooth={true} duration={600}>
+
+        <Link to="contact" spy={true} smooth={true} duration={600}>
           CONTACT
         </Link>
       </div>
