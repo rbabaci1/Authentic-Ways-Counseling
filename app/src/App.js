d14d338ccca545b97681de0ade525042cb72bb15
rabah babaci <4rabah@gmail.com>
2020-07-15 09:37:13 -0700

    add style to the NavBar

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Authentic Ways Counseling</h1>
-      <h2>Work in Progress!</h2>
+    <div className="App home">
+      <NavBar />
     </div>
   );
 }
