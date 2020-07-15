1d040afbb85fbf5d82e50113ff59c02d338ec2bb
rabah babaci <4rabah@gmail.com>
2020-07-15 09:55:12 -0700

    add mock text to all the sections

--- app/src/App.js
+++ app/src/App.js
@@ -5,6 +5,102 @@ function App() {
   return (
     <div className="App home">
       <NavBar />
+
+      <div className="about">
+        <h1>ABOUT ME</h1>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Sit amet cursus sit
+        amet. Pulvinar pellentesque habitant morbi tristique senectus et netus
+        et. Lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque
+        habitant. Tortor posuere ac ut consequat semper viverra nam libero.
+        Mattis nunc sed blandit libero. Posuere urna nec tincidunt praesent
+        semper feugiat. At tellus at urna condimentum. Velit egestas dui id
+        ornare arcu odio ut sem. Consequat mauris nunc congue nisi vitae
+        suscipit tellus mauris a. Egestas quis ipsum suspendisse ultrices
+        gravida dictum fusce ut. Quam lacus suspendisse faucibus interdum
+        posuere lorem. In arcu cursus euismod quis viverra. Non consectetur a
+        erat nam at. Enim tortor at auctor urna nunc id cursus metus. Quis
+        eleifend quam adipiscing vitae proin sagittis. Risus sed vulputate odio
+        ut enim. Ut enim blandit volutpat maecenas volutpat blandit aliquam
+        etiam. Ultrices eros in cursus turpis massa tincidunt dui ut. Iaculis
+        nunc sed augue lacus viverra vitae. Pellentesque id nibh tortor id
+        aliquet lectus. Sapien pellentesque habitant morbi tristique senectus
+        et. Volutpat lacus laoreet non curabitur gravida arcu. Auctor urna nunc
+        id cursus. Dui sapien eget mi proin sed. Gravida in fermentum et
+        sollicitudin.
+      </div>
+
+      <div className="help">
+        <h1>HOW I HELP</h1>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Sit amet cursus sit
+        amet. Pulvinar pellentesque habitant morbi tristique senectus et netus
+        et. Lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque
+        habitant. Tortor posuere ac ut consequat semper viverra nam libero.
+        Mattis nunc sed blandit libero. Posuere urna nec tincidunt praesent
+        semper feugiat. At tellus at urna condimentum. Velit egestas dui id
+        ornare arcu odio ut sem. Consequat mauris nunc congue nisi vitae
+        suscipit tellus mauris a. Egestas quis ipsum suspendisse ultrices
+        gravida dictum fusce ut. Quam lacus suspendisse faucibus interdum
+        posuere lorem. In arcu cursus euismod quis viverra. Non consectetur a
+        erat nam at. Enim tortor at auctor urna nunc id cursus metus. Quis
+        eleifend quam adipiscing vitae proin sagittis. Risus sed vulputate odio
+        ut enim. Ut enim blandit volutpat maecenas volutpat blandit aliquam
+        etiam. Ultrices eros in cursus turpis massa tincidunt dui ut. Iaculis
+        nunc sed augue lacus viverra vitae. Pellentesque id nibh tortor id
+        aliquet lectus. Sapien pellentesque habitant morbi tristique senectus
+        et. Volutpat lacus laoreet non curabitur gravida arcu. Auctor urna nunc
+        id cursus. Dui sapien eget mi proin sed. Gravida in fermentum et
+        sollicitudin.
+      </div>
+
+      <div className="resources">
+        <h1>RESOURCES</h1>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Sit amet cursus sit
+        amet. Pulvinar pellentesque habitant morbi tristique senectus et netus
+        et. Lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque
+        habitant. Tortor posuere ac ut consequat semper viverra nam libero.
+        Mattis nunc sed blandit libero. Posuere urna nec tincidunt praesent
+        semper feugiat. At tellus at urna condimentum. Velit egestas dui id
+        ornare arcu odio ut sem. Consequat mauris nunc congue nisi vitae
+        suscipit tellus mauris a. Egestas quis ipsum suspendisse ultrices
+        gravida dictum fusce ut. Quam lacus suspendisse faucibus interdum
+        posuere lorem. In arcu cursus euismod quis viverra. Non consectetur a
+        erat nam at. Enim tortor at auctor urna nunc id cursus metus. Quis
+        eleifend quam adipiscing vitae proin sagittis. Risus sed vulputate odio
+        ut enim. Ut enim blandit volutpat maecenas volutpat blandit aliquam
+        etiam. Ultrices eros in cursus turpis massa tincidunt dui ut. Iaculis
+        nunc sed augue lacus viverra vitae. Pellentesque id nibh tortor id
+        aliquet lectus. Sapien pellentesque habitant morbi tristique senectus
+        et. Volutpat lacus laoreet non curabitur gravida arcu. Auctor urna nunc
+        id cursus. Dui sapien eget mi proin sed. Gravida in fermentum et
+        sollicitudin.
+      </div>
+
+      <div className="contact">
+        <h1>CONTACT</h1>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Sit amet cursus sit
+        amet. Pulvinar pellentesque habitant morbi tristique senectus et netus
+        et. Lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque
+        habitant. Tortor posuere ac ut consequat semper viverra nam libero.
+        Mattis nunc sed blandit libero. Posuere urna nec tincidunt praesent
+        semper feugiat. At tellus at urna condimentum. Velit egestas dui id
+        ornare arcu odio ut sem. Consequat mauris nunc congue nisi vitae
+        suscipit tellus mauris a. Egestas quis ipsum suspendisse ultrices
+        gravida dictum fusce ut. Quam lacus suspendisse faucibus interdum
+        posuere lorem. In arcu cursus euismod quis viverra. Non consectetur a
+        erat nam at. Enim tortor at auctor urna nunc id cursus metus. Quis
+        eleifend quam adipiscing vitae proin sagittis. Risus sed vulputate odio
+        ut enim. Ut enim blandit volutpat maecenas volutpat blandit aliquam
+        etiam. Ultrices eros in cursus turpis massa tincidunt dui ut. Iaculis
+        nunc sed augue lacus viverra vitae. Pellentesque id nibh tortor id
+        aliquet lectus. Sapien pellentesque habitant morbi tristique senectus
+        et. Volutpat lacus laoreet non curabitur gravida arcu. Auctor urna nunc
+        id cursus. Dui sapien eget mi proin sed. Gravida in fermentum et
+        sollicitudin.
+      </div>
     </div>
   );
 }
--- app/src/components/NavBar/index.js
+++ app/src/components/NavBar/index.js
@@ -6,21 +6,23 @@ import "./navBar.scss";
 export default function NavBar() {
   return (
     <div className="navBar">
-      <Link to="home" smooth={true} duration={400}>
-        HOME
-      </Link>
-      <Link to="about" smooth={true} duration={600}>
-        ABOUT ME
-      </Link>
-      <Link to="help" smooth={true} duration={600}>
-        HOW I HELP
-      </Link>
-      <Link to="resources" smooth={true} duration={600}>
-        RESOURCES
-      </Link>
-      <Link to="contact" smooth={true} duration={600}>
-        CONTACT
-      </Link>
+      <div className="desktop">
+        <Link to="home" smooth={true} duration={400}>
+          HOME
+        </Link>
+        <Link to="about" smooth={true} duration={600}>
+          ABOUT ME
+        </Link>
+        <Link to="help" smooth={true} duration={600}>
+          HOW I HELP
+        </Link>
+        <Link to="resources" smooth={true} duration={600}>
+          RESOURCES
+        </Link>
+        <Link to="contact" smooth={true} duration={600}>
+          CONTACT
+        </Link>
+      </div>
     </div>
   );
 }
